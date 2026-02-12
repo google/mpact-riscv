a54b90f380f8845f8029c7b56ff7886908a6f942
MPACT-Sim team <no-reply@google.com>
2026-02-12 15:27:28 -0600

    No public description
    PiperOrigin-RevId: 869358877
    Change-Id: I87fffa11c0a243be548860cab6412fbc13e4515f

--- riscv/test/riscv_vector_memory_instructions_test.cc
+++ riscv/test/riscv_vector_memory_instructions_test.cc
@@ -20,19 +20,24 @@
 #include <functional>
 #include <ios>
 #include <string>
+#include <tuple>
+#include <type_traits>
 #include <vector>
 
 #include "absl/functional/bind_front.h"
 #include "absl/numeric/bits.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "googlemock/include/gmock/gmock.h"
 #include "mpact/sim/generic/data_buffer.h"
 #include "mpact/sim/generic/immediate_operand.h"
 #include "mpact/sim/generic/instruction.h"
+#include "mpact/sim/generic/register.h"
 #include "mpact/sim/util/memory/flat_demand_memory.h"
 #include "riscv/riscv_register.h"
 #include "riscv/riscv_state.h"
+#include "riscv/riscv_vector_state.h"
 
 // This file contains the test fixture and tests for testing RiscV vector
 // memory instructions.
